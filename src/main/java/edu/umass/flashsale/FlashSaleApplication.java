d558792dd52d23ff4316890e66eabd71b90c3d93
Govind Prasad Sahu <gp007s@Govinds-Laptop.local>
2025-10-16 21:47:06 -0400

    first commit

---
+++ src/main/java/edu/umass/flashsale/FlashSaleApplication.java
@@ -0,0 +1,20 @@
+package edu.umass.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FlashSaleApplication {
+
+	@GetMapping("/health")
+	public String checkAppHealth(){
+		return "FlashSaleApi is healthy!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleApplication.class, args);
+	}
+
+}
