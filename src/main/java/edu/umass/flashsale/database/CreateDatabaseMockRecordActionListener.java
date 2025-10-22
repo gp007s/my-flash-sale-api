e40e67f25aa8d2a6b64713d65f595bfb3ee9937b
Govind <gp007s@gmail.com>
2025-10-22 11:51:23 -0400

    fix- removing the default ddl statment

--- src/main/java/edu/umass/flashsale/database/CreateDatabaseMockRecordActionListener.java
+++ src/main/java/edu/umass/flashsale/database/CreateDatabaseMockRecordActionListener.java
@@ -1,22 +1,23 @@
 package edu.umass.flashsale.database;
 
-import edu.umass.flashsale.model.OrderfulfillmentData;
+//import edu.umass.flashsale.model.OrderfulfillmentData;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
 
 @Configuration
 public class CreateDatabaseMockRecordActionListener {
-   @Bean
+   //@Bean
     ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(OrderfulfillmentRepository repository) {
 
-        return event->repository
-                .saveAll(Stream.of("A", "B", "C").map(name -> new OrderfulfillmentData("order123455","item1","TV", 3,"Govind","gp007s@gmail.com", 200l,"FULFILLED")).collect(Collectors.toList()))
-                .forEach(System.out::println);
+       // return event->repository
+             //   .saveAll(Stream.of("A", "B", "C").map(name -> new OrderfulfillmentData("order123455","item1","TV", 3,"Govind","gp007s@gmail.com", 200l,"FULFILLED")).collect(Collectors.toList()))
+                //.forEach(System.out::println);
+        return null;
     }
 
 }
